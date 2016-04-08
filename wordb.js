7ee34c37c25f9a698db53f70a7f1c59507c021c2
Wouter van Dijke <woutervdijke@me.com>
2016-04-08 16:02:53 +0200

    added wordcounts to articleArray

--- wordb.js
+++ wordb.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var sugar = require('sugar');
 
 var collection = fs.readFileSync('collection.txt').toString(); //read the collection file
 collection = collection.replace(/(\r\n|\n|\r)|<\/p>|<p>|\(|\)|'|"|\.|\?/gm, "").toLowerCase(); //pre-process to remove interpunction, line breaks and uppercase letters.
@@ -10,15 +11,18 @@ var articleArray = [];
 for (var i=0; i < articleCount; i++) {
 	articleArray[i] = [articles[i].substring(articles[i].search('<docid>')+7, articles[i].search('</docid>')), articles[i].substring(articles[i].search('<text>')+6, articles[i].search('</text>'))];
 	}
-	
-var wordlist = [];
-for (var w=0; w < articleCount; w++) {
-	 wordlist[w] = articleArray[w][1].split(' ').sort();}
 
-console.log(wordlist[1]);
+for (var w=0; w < articleCount; w++) {
+	 articleArray[w][1] = articleArray[w][1].split(' ').sort();}
+
+
+for (var x=0; x < articleCount; x++) {
+	articleArray[x][2] = [];
+	for (var y=0; y < articleArray[x][1].length; y++) {
+			articleArray[x][2][y] = articleArray[x][1].count(articleArray[x][1][y]);
+		}
+	}
+
+console.log(articleArray[1][2]);
 
 //todo create object from wordlist
-
-var wordcounts = {Australia:[[0,7][1,0][2,3]]};
-
-// GitHub test
