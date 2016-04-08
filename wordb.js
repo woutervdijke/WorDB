042b63332b4557c41a01197c8496b785defec790
Wouter van Dijke <woutervdijke@me.com>
2016-04-09 01:36:21 +0200

    simplified code

--- wordb.js
+++ wordb.js
@@ -1,7 +1,6 @@
 var fs = require('fs'); // File System is required to read the collection file
 var sugar = require('sugar'); // Sugar enables the .count() method
-var _ = require('underscore'); // Underscore enables the .isEmpty() method
-var prompt = require('prompt');
+var prompt = require('prompt'); //
 
 var collection = fs.readFileSync('collection.txt').toString(); //read the collection file
 collection = collection.replace(/(\r\n|\n|\r)|<\/p>|<p>|\(|\)|'|"|,|:|\.|\?/gm, "").toLowerCase(); //pre-process to remove interpunction, line breaks and uppercase letters.
@@ -10,30 +9,21 @@ var articles = collection.split('</doc>'); // split the collection file into the
 var articleCount = articles.length-1; // for some reason articles.length is 1 higher than the actual number of articles
 var articleArray = [];
 
-// put all articles in an array
+// put all articles in an array and split the articles into lists of words
 for (var i=0; i < articleCount; i++) {
 	articleArray[i] = [articles[i].substring(articles[i].search('<docid>')+7, articles[i].search('</docid>')), articles[i].substring(articles[i].search('<text>')+6, articles[i].search('</text>'))];
-	}
+	articleArray[i][1] = articleArray[i][1].split(' ').sort();
+}
 
-// split the articles into lists of words
-for (var w=0; w < articleCount; w++) {
-	 articleArray[w][1] = articleArray[w][1].split(' ').sort();}
-
-// put the word count for each word in the array
+// put the word count and docid for each word in the array
 for (var x=0; x < articleCount; x++) {
 	articleArray[x][2] = [];
-	for (var y=0; y < articleArray[x][1].length; y++) {
-			articleArray[x][2][y] = articleArray[x][1].count(articleArray[x][1][y]);
-		}
-	}
-
-// repeat the docid for each word in the array
-for (var x=0; x < articleCount; x++) {
 	articleArray[x][3] = [];
 	for (var y=0; y < articleArray[x][1].length; y++) {
+		articleArray[x][2][y] = articleArray[x][1].count(articleArray[x][1][y]);
 		articleArray[x][3][y] = articleArray[x][0];
+		}
 	}
-}
 
 var wordlist = {}; // create an empty object to contain the wordlist
 
@@ -43,7 +33,8 @@ for (var x=0; x < articleCount; x++) {
 			wordlist[articleArray[x][1][y]+x] = {count: articleArray[x][2][y], docid: articleArray[x][3][y]};
 	}
 }
-// the prompt part makes it a bit more
+
+// the prompt part makes it a bit more interactive
 prompt.start();
 
 prompt.get(['wordToSearch'], function (err, result) {
